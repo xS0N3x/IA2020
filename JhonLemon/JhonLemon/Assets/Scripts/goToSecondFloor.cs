dea5b578ade5f7013ca3023109cfe1a32647a826
dazaichan <al375504@uji.es>
2020-10-20 18:49:56 +0200

    escenarioEnProceso
    Ahí va

---
+++ JhonLemon/JhonLemon/Assets/Scripts/goToSecondFloor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goToSecondFloor : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            SceneManager.LoadScene("piso2enEdicion");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
