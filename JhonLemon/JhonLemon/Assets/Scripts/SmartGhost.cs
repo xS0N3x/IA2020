4870bff54bc2afa49431f906eca9460ffe06039d
Mario <al375547@uji.es>
2020-11-01 18:59:33 +0100

    A* Following

---
+++ JhonLemon/JhonLemon/Assets/Scripts/SmartGhost.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartGhost : MonoBehaviour
+{
+    public Transform player;
+    public GameEnding gameEnding;
+    public Rigidbody rigidBody;
+    public float speed = 4f;
+
+    bool m_IsPlayerInRange;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    //gameEnding.CaughtPlayer();
+                    this.transform.position = raycastHit.point;
+                }
+            }
+        }
+    }
+}
