8f976eafc2e65144a27e8498d0db0737a12867dd
Borja Escrig Pérez <al315542@uji.es>
2020-11-05 09:14:08 +0100

    Gargolas
    Te detectan al tiempo con un indicador de luz

--- JhonLemon/JhonLemon/Assets/Scripts/Observer.cs
+++ JhonLemon/JhonLemon/Assets/Scripts/Observer.cs
@@ -7,7 +7,21 @@ public class Observer : MonoBehaviour
     public Transform player;
     public GameEnding gameEnding;
 
+    private SkinnedMeshRenderer light;
+    private Color lightColorCaught;
+    private Color lightColorInitial;
+    private Color lightColorChange;
+
     bool m_IsPlayerInRange;
+
+    private void Start()
+    {
+        light = transform.parent.gameObject.transform.GetComponentInChildren<SkinnedMeshRenderer>();
+        lightColorCaught = Color.red;
+        lightColorInitial = Color.green;
+        lightColorChange = lightColorInitial;
+        light.materials[0].SetColor("_EmissionColor", lightColorInitial);
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -28,6 +42,11 @@ public class Observer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (lightColorChange.r >= 0.95)
+        {
+            gameEnding.CaughtPlayer();
+        }
+
         if (m_IsPlayerInRange)
         {
             Vector3 direction = player.position - transform.position + Vector3.up;
@@ -37,9 +56,15 @@ public class Observer : MonoBehaviour
             {
                 if (raycastHit.collider.transform == player)
                 {
-                    gameEnding.CaughtPlayer();
+                    lightColorChange = Color.Lerp(lightColorChange, lightColorCaught, Time.deltaTime * 1.3f);
+                    light.materials[0].SetColor("_EmissionColor", lightColorChange);
                 }
             }
+        }
+        else
+        {
+            lightColorChange = Color.Lerp(lightColorChange, lightColorInitial, Time.deltaTime * 0.5f);
+            light.materials[0].SetColor("_EmissionColor", lightColorChange);
         }
     }
 }
